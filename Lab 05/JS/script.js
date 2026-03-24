a210c305d3d44f7b11723ee1e8dfcb42dd2a4812
Rifat9874 <rifat9461@gmail.com>
2026-03-24 12:16:21 +0600

    Fix text analysis functionality

--- Lab 05/JS/script.js
+++ Lab 05/JS/script.js
@@ -1,18 +1,18 @@
 function analyze() {
+    var text = document.getElementById("text").value.trim();
 
-    var text = document.getElementById("text").value;
-
-    
-    var charCount = text.length;
-
-
-    var wordCount;
-    if (text.trim() === "") {
-        wordCount = 0;
-    } else {
-        wordCount = text.trim().split(" ").length;
+    if (text === "") {
+        document.getElementById("char").innerText = "0";
+        document.getElementById("word").innerText = "0";
+        document.getElementById("reverse").innerText = "Please enter some text.";
+        return;
     }
 
+    var charCount = text.length;
+
+    var wordCount = text.split(/\s+/).filter(function(word) {
+        return word.length > 0;
+    }).length;
 
     var reverseText = text.split("").reverse().join("");
 
